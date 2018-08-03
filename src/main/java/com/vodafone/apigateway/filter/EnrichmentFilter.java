d56684095adc4bc7e94a9def98302fe4e54f03c3
La Torre <Filippo.La-Torre@Vodafone.com>
2018-08-03 18:07:19 +0200

    Rest clients

--- src/main/java/com/vodafone/apigateway/filter/EnrichmentFilter.java
+++ src/main/java/com/vodafone/apigateway/filter/EnrichmentFilter.java
@@ -1,9 +1,14 @@
 package com.vodafone.apigateway.filter;
 
 import com.google.common.base.Strings;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.web.client.RestTemplate;
 
 public class EnrichmentFilter extends ZuulFilter {
 
@@ -26,6 +31,22 @@ public class EnrichmentFilter extends ZuulFilter {
     @Override
     public Object run() {
         RequestContext context = RequestContext.getCurrentContext();
+
+        HttpResponse<JsonNode> jsonResponse = null;
+        try {
+            jsonResponse = Unirest.get("http://rest-service-jenkins.dxl-pre.vodafone.com/rest-service/greeting").asJson();
+            System.out.println(jsonResponse.getBody().toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            String result = restTemplate.getForObject("http://rest-service-jenkins.dxl-pre.vodafone.com/rest-service/greeting", String.class);
+            System.out.println(result);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
         if (!Strings.isNullOrEmpty(context.getRequest().getHeader("x-port-forwarded-for"))
                 && !Strings.isNullOrEmpty(context.getRequest().getHeader("x-forwarded-for"))) {
             System.out.println(context.getRequest().getHeader("x-port-forwarded-for"));
